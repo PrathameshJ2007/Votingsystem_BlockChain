0693241aa734c59cd1c2a38bd4a6c06d14dab2f1
PrathameshJ2007 <pjagtapb2007@gmail.com>
2026-04-15 13:23:47 +0530

    Add Block class with properties for index, timestamp, previousHash, hash, and data

---
+++ blockchain/Block.java
@@ -0,0 +1,10 @@
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+public class Block {
+    private int index;
+    private long timestamp;
+    private String previousHash;
+    private String hash;
+    private String data;
+
+}
