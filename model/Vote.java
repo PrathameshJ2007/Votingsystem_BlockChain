f75705c8070adb13d68f53c067d59d241380b182
PrathameshJ2007 <pjagtapb2007@gmail.com>
2026-04-15 13:21:03 +0530

    Add Vote class with properties for voterId and candidateId, including constructor and accessors

--- model/Vote.java
+++ model/Vote.java
@@ -1,3 +1,21 @@
 public class Vote {
-    
+    private String voterId;
+    private String candidateId;
+
+    public Vote(String voterId , String candidateId){
+        this.voterId = voterId;
+        this.candidateId = candidateId;
+    }
+    public String getVoterId() {
+        return voterId;
+    }
+
+    public String getCandidateId() {
+        return candidateId;
+    }
+    @Override
+    public String toString() {
+        return voterId + "->" + candidateId;
+    }
+
 }
